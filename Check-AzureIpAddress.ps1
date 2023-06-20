4cb0aa48273bb60bc83fc839b54dc09dfa29943e
Shuhei Uda <shuda@microsoft.com>
2023-06-20 18:43:16 +0900

    Ver.1.3.0

--- Check-AzureIpAddress.ps1
+++ Check-AzureIpAddress.ps1
@@ -11,31 +11,44 @@ Check-AzureIpAddress.ps1 -IpAddress 13.78.0.1, 13.78.0.2
 .NOTES
     Name    : Check-AzureIpAddress.ps1
     GitHub  : https://github.com/ShuheiUda/Check-AzureIpAddress
-    Version : 1.2.0
+    Version : 1.3.0
     Author  : Syuhei Uda
 #>
 Param(
-    [Parameter(Mandatory=$true)][array]$IpAddresses
+    [Parameter(Mandatory=$true)][array]$IpAddresses,
+    [switch]$UseOnlineCache
 
 )
 
-function Validate-StringIPv4Address{
+function Validate-StringIpAddress{
 Param(
     [Parameter(Mandatory=$true)]$IPv4Addresses
 )
-    $IPv4Addresses | foreach{
-        [int[]]$SplitIPv4Address = $_.Split(".")
-        if($SplitIPv4Address.Count -ne 4){
-            Return $false
-        }else{
-            for($octet = 0; $octet -lt 4; $octet++){
-                if(($SplitIPv4Address[$octet] -ge 0) -and ($SplitIPv4Address[$octet] -le 256)){
-                }else{
-                    Return $false
+    # IPv4
+    if($_ -ne $null -and !$_.Contains(":")){
+        $IPv4Addresses | foreach{
+            $SplitIPv4AddressPrefix = $_.Split("/")
+            [int[]]$SplitIPv4Address = $SplitIPv4AddressPrefix[0].Split(".")
+            if($SplitIPv4Address.Count -ne 4){
+                Return $false
+            }else{
+                for($octet = 0; $octet -lt 4; $octet++){
+                    if(($SplitIPv4Address[$octet] -ge 0) -and ($SplitIPv4Address[$octet] -le 255)){
+                    }else{
+                        Return $false
+                    }
                 }
             }
         }
     }
+
+    # IPv6
+    else
+    {
+        ### need a fix for IPv6 ###
+        Return $false
+    }
+    Return $true
 }
 
 function ConvertTo-UInt32IPv4Address{
@@ -44,13 +57,14 @@ Param(
 )
 
     [uint32]$UInt32IPv4Address = 0
-    [int[]]$SplitIPv4Address = $IPv4Address.Split(".")
+    [uint32[]]$SplitIPv4Address = $IPv4Address.Split(".")
     if($SplitIPv4Address.Count -ne 4){
         Return $false
     }else{
         for($octet = 0; $octet -lt 4; $octet++){
-            if(($SplitIPv4Address[$octet] -ge 0) -and ($SplitIPv4Address[$octet] -le 256)){
+            if(($SplitIPv4Address[$octet] -ge 0) -and ($SplitIPv4Address[$octet] -le 255)){
                 $UInt32IPv4Address += ($SplitIPv4Address[$octet]*([math]::Pow(256,3-$octet)))
+                #$UInt32IPv4Address += ($SplitIPv4Address[$octet] -shl 8*(3-$octet))
             }else{
                 Return $false
             }
@@ -59,12 +73,11 @@ Param(
     Return $UInt32IPv4Address
 }
 
-function ConvertTo-UInt32IPv4StartAddress{
+function ConvertTo-UInt32IPv4FirstAddress{
 Param(
     [Parameter(Mandatory=$true)]$IPv4AddressRange
 )
 
-    [uint32]$UInt32IPv4Address = 0
     $SplitIPv4AddressRange = $IPv4AddressRange.Split("/")
     [int]$SplitIPv4AddressPrefix = $SplitIPv4AddressRange[1]
 
@@ -76,20 +89,30 @@ Param(
     }
 }
 
-function ConvertTo-UInt32IPv4EndAddress{
+function ConvertTo-UInt32IPv4LastAddress{
 Param(
     [Parameter(Mandatory=$true)]$IPv4AddressRange
 )
 
-    [uint32]$UInt32IPv4Address = 0
     $SplitIPv4AddressRange = $IPv4AddressRange.Split("/")
     [int]$SplitIPv4AddressPrefix = $SplitIPv4AddressRange[1]
 
-    if(($SplitIPv4AddressPrefix -ge 0) -and ($SplitIPv4AddressPrefix -le 32)){
+    if(($SplitIPv4AddressRange[1] -ne $null) -and ($SplitIPv4AddressPrefix -ge 0) -and ($SplitIPv4AddressPrefix -le 32)){
         Return ((ConvertTo-UInt32IPv4Address $SplitIPv4AddressRange[0]) + [math]::Pow(2, 32 - $SplitIPv4AddressPrefix) - 1)
     }else{
         Write-Error "IPv4 Address Range is not correctly."
         Return -1
+    }
+}
+
+function ConvertTo-StringIPv4LastAddress{
+Param(
+    [Parameter(Mandatory=$true)]$IPv4AddressRange
+)
+
+    $IPv4LastAddress = ConvertTo-UInt32IPv4LastAddress $IPv4AddressRange
+    if($IPv4LastAddress -ne -1){
+        Return ((($IPv4LastAddress -shr 24) % 256), (($IPv4LastAddress -shr 16) % 256), (($IPv4LastAddress -shr 8) % 256), ($IPv4LastAddress % 256)) -join "."
     }
 }
 
@@ -109,7 +132,7 @@ Param(
 ### Main method
 
 # Header
-$Version = "1.2.0"
+$Version = "1.3.0"
 $LatestVersionUrl = "https://raw.githubusercontent.com/ShuheiUda/Check-AzureIpAddress/master/LatestVersion.txt"
 $IsAzureIp = $false
 $Region = $null
@@ -130,70 +153,121 @@ if (Get-Module -ListAvailable -Name Az.Network){
 }
 
 # Address check
-if((Validate-StringIPv4Address $IpAddresses) -eq $false){
+<#
+### need a fix for IPv6 ###
+if((Validate-StringIpAddress $IpAddresses) -eq $false){
     Write-Error "Please input source address correctly. (Example: 192.168.0.0)"
     Return
-}
+}#>
 
-# Get IP address range and service tags from Download Center
-$downloadUri = "https://www.microsoft.com/en-in/download/confirmation.aspx?id=56519"
-$downloadPage = Invoke-WebRequest -Uri $downloadUri -UseBasicParsing 
-$jsonFileUri = ($downloadPage.RawContent.Split('"') -like "https://*ServiceTags*")[0]
-$response = Invoke-WebRequest -Uri $jsonFileUri
-$jsonResponse = [System.Text.Encoding]::UTF8.GetString($response.Content) | ConvertFrom-Json
 
-# Generate Service Tag Int Table
-$SetviceTagTable = @()
-$jsonResponse.values | foreach{
-    $Tag = $_.Name
-    $_.properties.addressPrefixes | foreach{
-        # Check IP address
-        $SetviceTagTable += [PSCustomObject]@{
-            "Tag" = $Tag
-            "StartAddress" = (ConvertTo-UInt32IPv4StartAddress $_)
-            "EndAddress" = (ConvertTo-UInt32IPv4EndAddress $_)
+$AzurePrefixTable = @()
+
+# Use Online Cache
+if($UseOnlineCache){
+    $AzurePrefixTableUrl = "https://azureiprange.blob.core.windows.net/checkip/AzurePrefixTable.csv"
+    $AzurePrefixTableResponse = Invoke-WebRequest -Uri $AzurePrefixTableUrl -UseBasicParsing
+    $AzurePrefixTable = [System.Text.Encoding]::UTF8.GetString($AzurePrefixTableResponse.Content) | ConvertFrom-Csv
+
+}else{
+    # Get IP address range and service tags from Download Center
+    $downloadUri = "https://www.microsoft.com/en-in/download/confirmation.aspx?id=56519"
+    $downloadPage = Invoke-WebRequest -Uri $downloadUri -UseBasicParsing 
+    $jsonFileUri = ($downloadPage.RawContent.Split('"') -like "https://*ServiceTags*")[0]
+    $response = Invoke-WebRequest -Uri $jsonFileUri
+    $jsonResponse = [System.Text.Encoding]::UTF8.GetString($response.Content) | ConvertFrom-Json
+
+    # Generate Service Tag Int Table
+    $jsonResponse.values | foreach{
+        $Tag = $_.Name
+        $_.properties.addressPrefixes | foreach{
+            # IPv4
+            if(Validate-StringIpAddress $_){
+                $AzurePrefixTable += [PSCustomObject]@{
+                    "Type" = "ServiceTag"
+                    "Tag" = $Tag
+                    "CommunityName" = $null
+                    "CommunityValue" = $null
+                    "Prefix" = $_
+                    "FirstAddress" = (ConvertTo-UInt32IPv4FirstAddress $_)
+                    "LastAddress" = (ConvertTo-UInt32IPv4LastAddress $_)
+                }
+            }
+            # IPv6
+            else
+            {
+                ### need a fix for IPv6 ###
+            }
         }
     }
-}
 
-# Generate BGP Community Int Table
-if($AzModule){
-    # Az.Network module needed
-    $BgpCommunity = Get-AzBgpServiceCommunity
-    $BgpCommunityTable = @()
-    $BgpCommunity.BgpCommunities | foreach{
-        $CommunityName = $_.CommunityName
-        $CommunityValue = $_.CommunityValue
-        $_.CommunityPrefixes | foreach{
-            # Check IP address
-            $BgpCommunityTable += [PSCustomObject]@{
-                "CommunityName" = $CommunityName
-                "CommunityValue" = $CommunityValue
-                "StartAddress" = (ConvertTo-UInt32IPv4StartAddress $_)
-                "EndAddress" = (ConvertTo-UInt32IPv4EndAddress $_)
+    # Generate BGP Community Int Table
+    if($AzModule){
+        # Az.Network module needed
+        $BgpCommunity = Get-AzBgpServiceCommunity
+        $BgpCommunity.BgpCommunities | foreach{
+            $CommunityName = $_.CommunityName
+            $CommunityValue = $_.CommunityValue
+            $_.CommunityPrefixes | foreach{
+                # IPv4
+                if(Validate-StringIpAddress $_){
+                    $AzurePrefixTable += [PSCustomObject]@{
+                        "Type" = "BgpCommunity"
+                        "Tag" = $null
+                        "CommunityName" = $CommunityName
+                        "CommunityValue" = $CommunityValue
+                        "Prefix" = $_
+                        "FirstAddress" = (ConvertTo-UInt32IPv4FirstAddress $_)
+                        "LastAddress" = (ConvertTo-UInt32IPv4LastAddress $_)
+                    }
+                }
+                # IPv6
+                else
+                {
+                    ### need a fix for IPv6 ###
+                }
             }
         }
     }
 }
 
 # Check Service Tag and BGP Community
+$ResultTable = @()
 foreach($IpAddress in $IpAddresses){
     $IsAzureIp = $false
     $TargetAddress = ConvertTo-UInt32IPv4Address $IpAddress
-    $SetviceTagTable | foreach{
+    $AzurePrefixTable | where Type -eq "ServiceTag" | foreach{
         # Check IP address
-        if(Check-UInt32IPv4AddressRange -UInt32TargetIPv4Address $TargetAddress -UInt32StartIPv4Address $_.StartAddress -UInt32EndIPv4Address $_.EndAddress){
+        if(Check-UInt32IPv4AddressRange -UInt32TargetIPv4Address $TargetAddress -UInt32StartIPv4Address $_.FirstAddress -UInt32EndIPv4Address $_.LastAddress){
             Write-Host "$IpAddress is in $($_.Tag) service tags." -ForegroundColor Green
+            $ResultTable += [PSCustomObject]@{
+                "QueryAddress    " = $IpAddress
+                "Type            " = $_.Type
+                "Tag             " = $_.Tag
+                "CommunityName   " = $null
+                "CommunityValue  " = $null
+                "Prefix          " = $_.Prefix
+                "LastAddress      " = ConvertTo-StringIPv4LastAddress $_.Prefix
+            }
             $IsAzureIp = $true
         }
     }
 
     # Az.Network module needed
-    if($AzModule){
-        $BgpCommunityTable | foreach{
+    if($AzModule -or $UseOnlineCache){
+        $AzurePrefixTable | where Type -eq "BgpCommunity" | foreach{
             # Check IP address
-            if(Check-UInt32IPv4AddressRange -UInt32TargetIPv4Address $TargetAddress -UInt32StartIPv4Address $_.StartAddress -UInt32EndIPv4Address $_.EndAddress){
+            if(Check-UInt32IPv4AddressRange -UInt32TargetIPv4Address $TargetAddress -UInt32StartIPv4Address $_.FirstAddress -UInt32EndIPv4Address $_.LastAddress){
                 Write-Host "$IpAddress is in $($_.CommunityName) ($($_.CommunityValue)) BGP community." -ForegroundColor Green
+                $ResultTable += [PSCustomObject]@{
+                    "QueryAddress    " = $IpAddress
+                    "Type            " = $_.Type
+                    "Tag             " = $null
+                    "CommunityName   " = $_.CommunityName
+                    "CommunityValue  " = $_.CommunityValue
+                    "Prefix          " = $_.Prefix
+                    "LastAddress      " = ConvertTo-StringIPv4LastAddress $_.Prefix
+                }
                 $IsAzureIp = $true
             }
         }
@@ -204,3 +278,4 @@ foreach($IpAddress in $IpAddresses){
         Start-Process "https://db-ip.com/$IpAddress"
     }
 }
+$ResultTable | Format-Table
--- README.md
+++ README.md
@@ -1,24 +1,47 @@
 # Check-AzureIpAddress
 
 ## Description
+
 This tool is checking IP address which you inputted is in Azure.
 
 ## Usage
+
 1. Run PowerShell console
-2. Run Check-AzureIpAddress script (ex. Check-AzureIpAddress.ps1 -IpAddresses 13.78.0.1)
+1. Run Check-AzureIpAddress script (ex. Check-AzureIpAddress.ps1 -IpAddresses 13.78.0.1)
 
 ## Parameter
+
 * Required
-    * IpAddresses
-    
+  * IpAddresses
+
+* Optional
+  * UseOnlineCache
+
 ## Sample
-.\Check-AzureIpAddress
+
+```
+.\Check-AzureIpAddress -IpAddresses  8.8.8.8, 13.78.0.1 -UseOnlineCache
+
+8.8.8.8 is not in Azure.
+13.78.0.1 is in AzureCloud.japaneast service tags.
+13.78.0.1 is in AzureCloud service tags.
+13.78.0.1 is in Azure Japan East (12076:51012) BGP community.
+
+QueryAddress     Type             Tag                  CommunityName    CommunityValue   Prefix           LastAddress
+---------------- ---------------- ----------------     ---------------- ---------------- ---------------- -----------------
+13.78.0.1        ServiceTag       AzureCloud.japaneast                                   13.78.0.0/17     13.78.127.255
+13.78.0.1        ServiceTag       AzureCloud                                             13.78.0.0/17     13.78.127.255
+13.78.0.1        BgpCommunity                          Azure Japan East 12076:51012      13.78.0.0/18     13.78.63.255
+```
 
 ## Lincense
+
 Copyright (c) 2017 Syuhei Uda
 Released under the [MIT license](http://opensource.org/licenses/mit-license.php )
 
 ## Release Notes
+
+* 2023/06/20 Ver.1.3.0 : Add UseOnlineCache flag, temporary IPv6 error avoidance
 * 2019/07/10 Ver.1.2.0 : Add BGP community, Optimization
 * 2019/02/01 Ver.1.1.0 : Add Service Tags
 * 2018/09/16 Ver.1.0.0 : Add dbip function
